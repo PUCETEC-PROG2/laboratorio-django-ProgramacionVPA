361d4bdaca9660adcd7c985bf9305492de555515
Victor Alvarez <VPALVAREZ@puce.edu.ec>
2025-01-11 18:19:38 -0500

    Taller 9

--- pokedex/admin.py
+++ pokedex/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import Pokemon
+from .models import Pokemon, Trainer
 
 # Register your models here.
 @admin.register(Pokemon)
 
 class PokemonAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Trainer)
+
+class TrainerAdmin(admin.ModelAdmin):
     pass
 
